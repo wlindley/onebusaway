f6cfdaa1ec246de48d51c5f4bfd5bc58c1f5201b
wlindley <walker@walkerlindley.com>
2014-03-15 13:59:27 -0700

    Refactoring code to separate out a function that does the work and another piece that handles command-line parameters.

--- getNextArrivalAtStop.py
+++ getNextArrivalAtStop.py
@@ -5,58 +5,53 @@ import sys
 import json
 import datetime
 
-if len(sys.argv) < 2:
-	print "--"
-	sys.exit(-1)
+def getNextArrivalInMinutes(apiKey, stopId, busId=None):
+	url = "http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_%s.json?minutesBefore=0&minutesAfter=99&key=%s" % (stopId, apiKey)
+	responseHandle = urllib2.urlopen(url)
+	response = json.loads(responseHandle.read())
 
-stopId = sys.argv[1]
-busId = None
-if len(sys.argv) > 2:
-	busId = sys.argv[2]
+	currentTime = response["currentTime"]
+	arrivals = response["data"]["arrivalsAndDepartures"]
 
-url = "http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_%s.json?minutesBefore=0&minutesAfter=99&key=TEST" % stopId
-responseHandle = urllib2.urlopen(url)
-response = json.loads(responseHandle.read())
-
-currentTime = response["currentTime"]
-arrivals = response["data"]["arrivalsAndDepartures"]
-
-if len(arrivals) <= 0:
-	print "--"
-	sys.exit(-1)
+	if len(arrivals) <= 0:
+		raise Exception("No upcoming arrivals at stop %s" % stopId)
 	
-soonestTime = None
-for arrival in arrivals:
-	name = arrival["routeShortName"]
-	if None != busId and name != busId:
-		continue
+	soonestTime = None
+	for arrival in arrivals:
+		name = arrival["routeShortName"]
+		if None != busId and name != busId:
+			continue
 	
-	predicted = arrival["predictedArrivalTime"]
-	scheduled = arrival["scheduledArrivalTime"]
-	if scheduled > currentTime and (scheduled < soonestTime or soonestTime == None):
-		soonestTime = scheduled
-	if predicted > currentTime and (predicted < soonestTime or soonestTime == None):
-		soonestTime = predicted
+		predicted = arrival["predictedArrivalTime"]
+		scheduled = arrival["scheduledArrivalTime"]
+		if scheduled > currentTime and (scheduled < soonestTime or soonestTime == None):
+			soonestTime = scheduled
+		if predicted > currentTime and (predicted < soonestTime or soonestTime == None):
+			soonestTime = predicted
 
-if not soonestTime:
-	print "--"
-	sys.exit(-1)
+	if not soonestTime:
+		raise Exception("No upcoming arrivals of bus %s at stop %s" % (busId, stopId))
 
-current = datetime.datetime.fromtimestamp(currentTime / 1000) #convert ms to s
-soonest = datetime.datetime.fromtimestamp(soonestTime / 1000) #convert ms to s
+	current = datetime.datetime.fromtimestamp(currentTime / 1000) #convert ms to s
+	soonest = datetime.datetime.fromtimestamp(soonestTime / 1000) #convert ms to s
 
-nextArrival = int((soonest - current).total_seconds() / 60) #convert sec to min
-print nextArrival
-sys.exit(nextArrival)
+	return int((soonest - current).total_seconds() / 60) #convert sec to min
 
-#print "Current time:"
-#print currentTime
-#print currentTime / 1000
-#print current.isoformat()
-#print "Next arrival:"
-#print soonestTime
-#print soonestTime / 1000
-#print soonest.isoformat()
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "--"
+		sys.exit(-1)
 
-#print "Data:"
-#print json.dumps(response["data"]["arrivalsAndDepartures"], indent=1)
+	stopId = sys.argv[1]
+	busId = None
+	if len(sys.argv) > 2:
+		busId = sys.argv[2]
+
+	apiKey = "TEST"
+	try:
+		nextArrival = getNextArrivalInMinutes(apiKey, stopId, busId)
+		print nextArrival
+		sys.exit(nextArrival)
+	except Exception as e:
+		print "--"
+		sys.exit(255)
