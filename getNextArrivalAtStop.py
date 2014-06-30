84441312d462104cb07c481c459e5d00fbeb6900
wlindley <walker@walkerlindley.com>
2014-06-29 20:00:21 -0700

    Adding support for for specifying the index of arrival the requester is interested in.

--- getNextArrivalAtStop.py
+++ getNextArrivalAtStop.py
@@ -15,7 +15,7 @@ def getAPIKey(filename):
 	except:
 		return "TEST"
 
-def getNextArrivalInSeconds(apiKey, stopId, busId=None):
+def getNextArrivalInSeconds(apiKey, stopId, busId=None, arrivalIndex=0):
 	url = "http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_%s.json?minutesBefore=0&minutesAfter=99&key=%s" % (stopId, apiKey)
 	responseHandle = urllib2.urlopen(url)
 	response = json.loads(responseHandle.read())
@@ -35,21 +35,26 @@ def getNextArrivalInSeconds(apiKey, stopId, busId=None):
 	if len(arrivals) <= 0:
 		raise Exception("No upcoming arrivals at stop %s" % stopId)
 	
-	soonestTime = None
+	soonestTimes = []
 	for arrival in arrivals:
 		name = arrival["routeShortName"]
 		if None != busId and name != busId:
 			continue
 	
+		curArrival = arrival["scheduledArrivalTime"]
 		predicted = arrival["predictedArrivalTime"]
-		scheduled = arrival["scheduledArrivalTime"]
-		if scheduled > currentTime and (scheduled < soonestTime or soonestTime == None):
-			soonestTime = scheduled
-		if predicted > currentTime and (predicted < soonestTime or soonestTime == None):
-			soonestTime = predicted
+		if predicted < curArrival:
+			curArrival = predicted
+		soonestTimes.append(curArrival)
 
-	if not soonestTime:
+	soonestTimes.sort()
+
+	if 0 == len(soonestTimes):
 		raise Exception("No upcoming arrivals of bus %s at stop %s" % (busId, stopId))
+	if arrivalIndex >= len(soonestTimes):
+		raise Exception("Stop %s only has %s upcoming arrivals for bus %s, but arrival index %s was requested" % (stopId, busId, len(soonestTimes), arrivalIndex))
+
+	soonestTime = soonestTimes[arrivalIndex]
 
 	current = datetime.datetime.fromtimestamp(currentTime / 1000) #convert ms to s
 	soonest = datetime.datetime.fromtimestamp(soonestTime / 1000) #convert ms to s
@@ -59,18 +64,25 @@ def getNextArrivalInSeconds(apiKey, stopId, busId=None):
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print float("NaN")
-		sys.exit(1)
+		sys.exit(2)
 
 	stopId = sys.argv[1]
 	busId = None
 	if len(sys.argv) > 2:
 		busId = sys.argv[2]
+	arrivalIndex = 0
+	if len(sys.argv) > 3:
+		try:
+			arrivalIndex = int(sys.argv[3])
+		except Exception as e:
+			print float("NaN")
+			sys.exit(3)
 
 	apiKey = getAPIKey("api.key")
 	try:
-		nextArrival = getNextArrivalInSeconds(apiKey, stopId, busId)
+		nextArrival = getNextArrivalInSeconds(apiKey, stopId, busId, arrivalIndex)
 		print nextArrival
 		sys.exit(0)
 	except Exception as e:
 		print float("NaN")
-		sys.exit(0)
+		sys.exit(1)
